c3e7e6e172433bf1c187e253f252ab72b164bd99
Kman1552 <kaviish08@gmail.com>
2026-01-05 22:35:10 +0530

    getting data out of API to then save to json

--- val.py
+++ val.py
@@ -2,20 +2,30 @@ import os
 from openaq import OpenAQ
 from pydantic import BaseModel
 from dotenv import load_dotenv
+import json
 
-load_dotenv()
-apiKey = os.getenv("OPENAQ_API_KEY")
-try:
-    client = OpenAQ(api_key=apiKey)
-    client.locations.get(8118)
+folder_path = "data/raw"
+file_name = "location_8118.json"
+full_path = os.path.join(folder_path, file_name)
 
-    limit = 1000
-    client.close()
-    print("Connection Successful")
-except Exception as e:
-    print("Connection Failed:", e)
+# #load_dotenv()
+# apiKey = os.getenv("OPENAQ_API_KEY")
+# try:
+#     client = OpenAQ(api_key=apiKey)
+#     response = client.locations.get(8118).dict()
+#     client.close()
+#     print("Connection Successful")
+# except Exception as e:
+#     print("Connection Failed:", e)
+
+
+with open(full_path, 'w') as file:
+    data = json.dump(response, file, indent=4)
+print("Data saved to", full_path)
 
 class CoordinateM(BaseModel):
 
     Lat: float
     Lon : float
+
+
